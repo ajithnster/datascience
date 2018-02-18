adde92f1d7c4087eeecc63ff6d153f09f80c4317
ajithnster <ajithnster@gmail.com>
2018-02-18 22:03:51 +0530

    Update MayaOcss.R

--- MayaOcss.R
+++ MayaOcss.R
@@ -10,15 +10,13 @@ myFirstFun<-function(filename,startTab,endTab)
 
 	print(paste("Sheet = ",iter))
 	temp_data = read.xlsx(filename, iter, startRow = 1) # open the file 
-	temp <- temp_data[,3:ncol(temp_data)]
+	temp <- temp_data[,1:ncol(temp_data)]
 	data <- cbind(data,temp)
  }
  final <- data
- final <- final[-1,]
- final <- final[-1,]
- final <- final[,-1]
+
  out_file = paste("out_",substr(filename, 1, nchar(filename)-5),".csv",sep = "" )
  write.csv(final,file=out_file)
 }
-myFirstFun("ocss17febFCS.xlsx",2,5)
+myFirstFun("ocss17feb.xlsx",2,30)
 View(final)
