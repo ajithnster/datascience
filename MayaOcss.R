2c2c657edbaea6286789879f387b98b6b8a61895
ajithnster <ajithnster@gmail.com>
2018-02-19 12:07:42 +0530

    Update MayaOcss.R

--- MayaOcss.R
+++ MayaOcss.R
@@ -16,15 +16,28 @@ myFirstFun<-function(filename,startTab,endTab)
  final <- data
 
  out_file = paste("out_",substr(filename, 1, nchar(filename)-5),".csv",sep = "" )
+if (file.exists(out_file)) file.remove(out_file)	
  write.csv(final,file=out_file)
+	
 }
 myFirstFun("ocss17feb.xlsx",2,30)
-View(final)
 
 library(data.table) 
-trdf = read.csv("out_ocss17feb1.csv")
+trdf = read.csv("out_ocss17feb.csv")
 df = transpose(trdf)
 df = df[complete.cases(df[,6]),]
-
-
-
+df= df[-1,-1]
+title = df[1,]
+df = df[complete.cases(df[,2:3]),]
+df = rbind(title,df)
+mylist = list()
+mylist["# Mentored"] = 6
+mylist["Total Registered"] = 10
+mylist["Part RF Call - late"] = 13
+mylist["Parent RF Call - late"] = 16
+mylist["Mentor RF Call - late"] = 18
+mylist["# CIFs Comp"] = 24
+numcols = ncol(df)
+required = cbind(df[1:3],df[,mylist[[1]]],df[,mylist[[2]]],df[,mylist[[3]]],df[,mylist[[4]]],df[,mylist[[5]]],df[,mylist[[6]]])
+View(required)
+write.csv(required,"final.out.csv")
